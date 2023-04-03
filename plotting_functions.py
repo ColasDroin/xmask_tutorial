489f86e917eded17b1b448c7e560d889a7f68fb8
Colas <colasdroin@gmail.com>
2023-04-03 17:11:13 +0200

    First commit tutorial xmask

---
+++ plotting_functions.py
@@ -0,0 +1,229 @@
+#################### Imports ####################
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+
+#################### Functions ####################
+def plot_all(tw_part, tw_other=None, name_1="beam 1", name_2="beam_2"):
+    # Build figure
+    fig = make_subplots(rows=3, cols=1, shared_xaxes=True)
+    fig.append_trace(
+        go.Scatter(
+            x=tw_part["s"],
+            y=tw_part["betx"],
+            mode="lines",
+            showlegend=True,
+            name=r"$\beta_x$ " + name_1,
+            # legendgroup="1",
+        ),
+        row=1,
+        col=1,
+    )
+
+    fig.append_trace(
+        go.Scatter(
+            x=tw_part["s"],
+            y=tw_part["bety"],
+            mode="lines",
+            showlegend=True,
+            name=r"$\beta_y$ " + name_1,
+            # legendgroup="1",
+        ),
+        row=1,
+        col=1,
+    )
+
+    if tw_other is not None:
+        fig.append_trace(
+            go.Scatter(
+                x=tw_other["s"],
+                y=tw_other["betx"],
+                mode="lines",
+                showlegend=True,
+                name=r"$\beta_x$ " + name_2,
+                # legendgroup="1",
+            ),
+            row=1,
+            col=1,
+        )
+
+        fig.append_trace(
+            go.Scatter(
+                x=tw_other["s"],
+                y=tw_other["bety"],
+                mode="lines",
+                showlegend=True,
+                name=r"$\beta_y$ " + name_2,
+                # legendgroup="1",
+            ),
+            row=1,
+            col=1,
+        )
+
+    fig.append_trace(
+        go.Scatter(
+            x=tw_part["s"],
+            y=tw_part["x"],
+            mode="lines",
+            showlegend=True,
+            name=r"$x$ " + name_1,
+            xaxis="x",
+            yaxis="y2",
+            # legendgroup="2",
+        ),
+        row=2,
+        col=1,
+    )
+
+    fig.append_trace(
+        go.Scatter(
+            x=tw_part["s"],
+            y=tw_part["y"],
+            mode="lines",
+            showlegend=True,
+            name=r"$y$ " + name_1,
+            xaxis="x",
+            yaxis="y2",
+            # legendgroup="2",
+        ),
+        row=2,
+        col=1,
+    )
+    if tw_other is not None:
+        fig.append_trace(
+            go.Scatter(
+                x=tw_other["s"],
+                y=tw_other["x"],
+                mode="lines",
+                showlegend=True,
+                name=r"$x$ " + name_2,
+                xaxis="x",
+                yaxis="y2",
+                # legendgroup="2",
+            ),
+            row=2,
+            col=1,
+        )
+
+        fig.append_trace(
+            go.Scatter(
+                x=tw_other["s"],
+                y=tw_other["y"],
+                mode="lines",
+                showlegend=True,
+                name=r"$y$ " + name_2,
+                xaxis="x",
+                yaxis="y2",
+                # legendgroup="2",
+            ),
+            row=2,
+            col=1,
+        )
+
+    fig.append_trace(
+        go.Scatter(
+            x=tw_part["s"],
+            y=tw_part["dx"],
+            mode="lines",
+            showlegend=True,
+            name=r"$D_x$ " + name_1,
+            xaxis="x",
+            yaxis="y3",
+            # legendgroup="3",
+        ),
+        row=3,
+        col=1,
+    )
+
+    fig.append_trace(
+        go.Scatter(
+            x=tw_part["s"],
+            y=tw_part["dy"],
+            mode="lines",
+            showlegend=True,
+            name=r"$D_y$ " + name_1,
+            xaxis="x",
+            yaxis="y3",
+            # legendgroup="3",
+        ),
+        row=3,
+        col=1,
+    )
+    if tw_other is not None:
+        fig.append_trace(
+            go.Scatter(
+                x=tw_other["s"],
+                y=tw_other["dx"],
+                mode="lines",
+                showlegend=True,
+                name=r"$D_x$ " + name_2,
+                xaxis="x",
+                yaxis="y3",
+                # legendgroup="3",
+            ),
+            row=3,
+            col=1,
+        )
+
+        fig.append_trace(
+            go.Scatter(
+                x=tw_other["s"],
+                y=tw_other["dy"],
+                mode="lines",
+                showlegend=True,
+                name=r"$D_y$ " + name_2,
+                xaxis="x",
+                yaxis="y3",
+                # legendgroup="3",
+            ),
+            row=3,
+            col=1,
+        )
+
+    # Update overall layout
+    fig.update_layout(
+        # title_text=r"$q_x = " + f'{tw_part["qx"]:.5f}' + r"\hspace{0.5cm}" + r" q_y = "
+        # f'{tw_part["qy"]:.5f}' + r"\hspace{0.5cm}" + r"Q'_x = "
+        # f'{tw_part["dqx"]:.2f}' + r"\hspace{0.5cm}" + r" Q'_y = "
+        # f'{tw_part["dqy"]:.2f}'
+        # +r"\hspace{0.5cm}" + r" \gamma_{tr} = "
+        # + f'{1/np.sqrt(tw_part["momentum_compaction_factor"]):.2f}'
+        # + r"$",  # "Transverse dynamics evolution with crossing angle",
+        title_x=0.5,
+        showlegend=True,
+        xaxis_showgrid=True,
+        yaxis_showgrid=True,
+        # xaxis_title=r'$s$',
+        # yaxis_title=r'$[m]$',
+        width=1000,
+        height=1000,
+        legend_tracegroupgap=200,
+        # dragmode="pan",
+        template="plotly_white",
+        # uirevision="Don't change",
+    )
+
+    # Update yaxis properties
+    fig.update_yaxes(
+        title_text=r"$\beta_{x,y}$ [m]", row=1, col=1
+    )  # , range=[0, 10000], row=1, col=1)
+    fig.update_yaxes(
+        title_text=r"(Closed orbit)$_{x,y}$ [m]", row=2, col=1
+    )  # , range=[-0.05, 0.05], row=2, col=1)
+    fig.update_yaxes(
+        title_text=r"$D_{x,y}$ [m]", range=[-1.5, 2.5], row=3, col=1
+    )  # , row=3, col=1)
+    fig.update_xaxes(title_text=r"$s$", row=3, col=1)
+    # fig.update_yaxes(fixedrange=True)
+
+    # # Add range slider
+    # fig.update_layout(
+    #     xaxis=dict(
+    #         rangeslider=dict(visible=True),
+    #         type="linear",
+    #         range=(10, 20),
+    #     )
+    # )
+
+    return fig
